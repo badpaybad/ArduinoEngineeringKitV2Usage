ec1fb65305029c20d2173f3a21d8e09d95f1ce48
Nguyen Phan Du <badpaybad@gmail.com>
2023-10-06 20:24:13 +0700

    https://github.com/arduino-libraries/ArduinoMotorCarrier

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ArduinoEngineeringKitV2Usage 
+
+https://github.com/arduino-libraries/ArduinoMotorCarrier
+
+
+https://docs.arduino.cc/tutorials/nano-33-iot/imu-accelerometer
+
+https://www.arduino.cc/reference/en/#functions
--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -5,7 +5,7 @@
 //Variable to store the battery voltage
 static float batteryVoltage;
 //low battery limit (discharged)
-static float batteryLimit; 
+static float batteryLimit;
 //Variable to change the motor speed and direction
 int duty = 50;
 
@@ -14,22 +14,20 @@ int degreesX = 0;
 int degreesY = 0;
 float x, y, z;
 
-void setup()
-{
+void setup() {
   //Serial port initialization
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   IMU.begin();
   //Establishing the communication with the motor shield
-  if (controller.begin())
-  {
+  if (controller.begin()) {
     Serial.print("MKR Motor Shield connected, firmware version ");
     Serial.println(controller.getFWVersion());
-  }
-  else
-  {
+  } else {
     Serial.println("Couldn't connect! Is the red led blinking? You may need to update the firmware with FWUpdater sketch");
-    while (1);
+    while (1)
+      ;
   }
 
   // Reboot the motor controller; brings every value back to default
@@ -47,10 +45,9 @@ void setup()
   M3.setDuty(0);
   M4.setDuty(0);
 
-  batteryLimit = 9.50; //Around 9V for a 3S LiPo battery
-
+  batteryLimit = 9.50;  //Around 9V for a 3S LiPo battery
 }
-int direction=1;
+int direction = 1;
 
 int encoderCount;
 
@@ -60,8 +57,7 @@ void loop() {
   batteryVoltage = (float)battery.getRaw() / 77;
 
   //Reset to the default values if the battery levels are lower than 11V
-  if (batteryVoltage < batteryLimit)
-  {
+  if (batteryVoltage < batteryLimit) {
     Serial.println(" ");
     Serial.println("WARNING: LOW BATTERY");
     Serial.println("ALL SYSTEMS DOWN");
@@ -69,93 +65,85 @@ void loop() {
     M2.setDuty(0);
     M3.setDuty(0);
     M4.setDuty(0);
-    while (batteryVoltage < batteryLimit) 
-    {
+    while (batteryVoltage < batteryLimit) {
       batteryVoltage = (float)battery.getRaw() / 77;
       controller.ping();
       //wait
       delay(50);
     }
-  }
-  else
-  {
+  } else {
     //Motor test
-    M1.setDuty(25);
-    delay(50);
+    //M1.setDuty(25);
+    //delay(50);
 
     float batteryVoltage = (float)battery.getRaw() / 77;
     //Serial.print("Battery voltage: ");
     //Serial.println(batteryVoltage);
 
-    servo3.setAngle(60- direction *30);
+    //servo3.setAngle(60- direction *30);
 
-    direction=direction*-1;
+    direction = direction * -1;
+  }
 
-    if (IMU.accelerationAvailable()) {
+  if (IMU.accelerationAvailable()) {
 
     IMU.readAcceleration(x, y, z);
 
+    if (x > 0.1) {
+
+      x = 100 * x;
+
+      degreesX = map(x, 0, 97, 0, 90);
+
+      Serial.print("Tilting up ");
+
+      Serial.print(degreesX);
+
+      Serial.println("  degrees");
+    }
+
+    if (x < -0.1) {
+
+      x = 100 * x;
+
+      degreesX = map(x, 0, -100, 0, 90);
+
+      Serial.print("Tilting down ");
+
+      Serial.print(degreesX);
+
+      Serial.println("  degrees");
+    }
+
+    if (y > 0.1) {
+
+      y = 100 * y;
+
+      degreesY = map(y, 0, 97, 0, 90);
+
+      Serial.print("Tilting left ");
+
+      Serial.print(degreesY);
+
+      Serial.println("  degrees");
+    }
+
+    if (y < -0.1) {
+
+      y = 100 * y;
+
+      degreesY = map(y, 0, -100, 0, 90);
+
+      Serial.print("Tilting right ");
+
+      Serial.print(degreesY);
+
+      Serial.println("  degrees");
+    }
 
   }
-
-
-  if (x > 0.1) {
-
-    x = 100 * x;
-
-    degreesX = map(x, 0, 97, 0, 90);
-
-    Serial.print("Tilting up ");
-
-    Serial.print(degreesX);
-
-    Serial.println("  degrees");
-
-  }
-
-  if (x < -0.1) {
-
-    x = 100 * x;
-
-    degreesX = map(x, 0, -100, 0, 90);
-
-    Serial.print("Tilting down ");
-
-    Serial.print(degreesX);
-
-    Serial.println("  degrees");
-
-  }
-
-  if (y > 0.1) {
-
-    y = 100 * y;
-
-    degreesY = map(y, 0, 97, 0, 90);
-
-    Serial.print("Tilting left ");
-
-    Serial.print(degreesY);
-
-    Serial.println("  degrees");
-
-  }
-
-  if (y < -0.1) {
-
-    y = 100 * y;
-
-    degreesY = map(y, 0, -100, 0, 90);
-
-    Serial.print("Tilting right ");
-
-    Serial.print(degreesY);
-
-    Serial.println("  degrees");
-
-  }
-
-  encoderCount= encoder1.getRawCount();
+  
+    encoderCount = encoder1.getRawCount();
     Serial.print("encoderCount ");
 
     Serial.print(encoderCount);
@@ -166,5 +154,4 @@ void loop() {
     controller.ping();
     //wait
     delay(50);
-  }
 }
